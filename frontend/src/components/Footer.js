f2f90e2350d064035da523d7559d4f2dd05295a4
Rudy-SEO <rudy@seomarketing.com>
2025-03-13 13:46:03 -0400

    Add Footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+
+const Footer = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: 'auto',
+        backgroundColor: (theme) => theme.palette.grey[200],
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography variant="body2" color="text.secondary" align="center">
+          {'© '}
+          {new Date().getFullYear()}
+          {' '}
+          <Link color="inherit" href="https://github.com/Rudy-SEO/BRD-Creator">
+            BRD Creator
+          </Link>
+          {' - AI-powered Business Requirements Document Generator'}
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
